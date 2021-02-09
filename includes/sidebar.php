fe71b97bdcd9fd060a4d24ee482f32e5037275f8
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2021-02-09 20:49:54 +0700

    fixing social media icon dan hide slider

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -47,11 +47,8 @@
 	    	<h3 class='box-title'><b class="util-text-white">Follow us on Social Media</b></h3>
 	  	</div>
 	  	<div class='box-body'>
-	    	<a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
-	    	<a class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
-	    	<a class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
-	    	<a class="btn btn-social-icon btn-google"><i class="fa fa-google-plus"></i></a>
-	    	<a class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
+	    	<a href="https://www.instagram.com/tedxits/" class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
+            <a href="https://www.linkedin.com/company/tedxits/" class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
 	  	</div>
 	</div>
 </div>
--- index.php
+++ index.php
@@ -22,30 +22,30 @@
 	        				unset($_SESSION['error']);
 	        			}
 	        		?>
-	        		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		                <ol class="carousel-indicators">
-		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
-		                </ol>
-		                <div class="carousel-inner">
-		                  <div class="item active">
-		                    <img src="images/banner1.png" alt="First slide">
-		                  </div>
-		                  <div class="item">
-		                    <img src="images/banner2.png" alt="Second slide">
-		                  </div>
-		                  <div class="item">
-		                    <img src="images/banner3.png" alt="Third slide">
-		                  </div>
-		                </div>
-		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-		                  <span class="fa fa-angle-left"></span>
-		                </a>
-		                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-		                  <span class="fa fa-angle-right"></span>
-		                </a>
-		            </div>
+<!--	        		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">-->
+<!--		                <ol class="carousel-indicators">-->
+<!--		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>-->
+<!--		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>-->
+<!--		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>-->
+<!--		                </ol>-->
+<!--		                <div class="carousel-inner">-->
+<!--		                  <div class="item active">-->
+<!--		                    <img src="images/banner1.png" alt="First slide">-->
+<!--		                  </div>-->
+<!--		                  <div class="item">-->
+<!--		                    <img src="images/banner2.png" alt="Second slide">-->
+<!--		                  </div>-->
+<!--		                  <div class="item">-->
+<!--		                    <img src="images/banner3.png" alt="Third slide">-->
+<!--		                  </div>-->
+<!--		                </div>-->
+<!--		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">-->
+<!--		                  <span class="fa fa-angle-left"></span>-->
+<!--		                </a>-->
+<!--		                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">-->
+<!--		                  <span class="fa fa-angle-right"></span>-->
+<!--		                </a>-->
+<!--		            </div>-->
 		            <h2>Monthly Top Items</h2>
 		       		<?php
 		       			$month = date('m');
